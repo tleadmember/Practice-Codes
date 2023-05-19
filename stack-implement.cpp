1edaf33adb1040491105e35906331dd50dbb18ed
tleadmember <thong.q.huynh@gmail.com>
2023-05-19 07:50:19 -0400

    First commit of stack-implement.cpp

---
+++ stack-implement.cpp
@@ -0,0 +1,36 @@
+/*
+TQBH - 2023-05-19
+Stack Implementation Using C++ Class
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
+// Define function to print an array
+void printarray(int array[], int length) {
+  using std::cout;
+  using std::endl;
+  for (int index = 0; index < length; ++index) {
+    cout << array[index] << " ";
+  }
+  cout << endl;
+}
+
+
+//
+
+
+
+
+// Main function
+int main() {
+  using std::cout;
+  using std::endl;
+  
+  
+
+
+  return 0;
+}
