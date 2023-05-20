e5d11184209aa35f90b5698615e8493031c27168
tleadmember <thong.q.huynh@gmail.com>
2023-05-20 01:19:03 -0400

    Commit of stack-implement.cpp after partially working

--- stack-implement.cpp
+++ stack-implement.cpp
@@ -1,5 +1,5 @@
 /*
-TQBH - 2023-05-19
+TQBH - 2023-05-19,20
 Stack Implementation Using C++ Class
 */
 
@@ -19,26 +19,68 @@ void printarray(int array[], int length) {
 }
 
 
-// Define the default size of the stack
-#define DEFAULT_SIZE = 10;
+// Define the default size of the stack (constant)
+#define DEFAULT_SIZE 10
 
 
 // Define stack using C++ class
 class Stack {
+  int* arr;  // if no access specifier = private access specifier
+  int top;
+  int capacity;
+  
 public:
-  Stack();  // constructor, same name as class, called when an object
-	    // of this class is created
+  Stack(int size = DEFAULT_SIZE);  // constructor, same name as class,
+				   // called when an object is created
   ~Stack();  // destructor, called when an object goes out of scope
 
-  //Define common methods for a stack
+  // Define common methods for a stack
+  int size();
   void push(int);
-  int pop();
-  int peek();
-
+  //int pop();
+  //int peek();
   bool isFull();
-  bool isEmpty();
-  
+  //bool isEmpty();
 };
+
+
+// Define constructor, outside of class body
+Stack::Stack(int size) {
+  arr = new int[size];
+  top = -1;
+  capacity = size;
+}
+
+
+// Define destructor
+Stack::~Stack() {
+  delete[] arr;  // to avoid memory leak after new opeator, memory reserved
+}
+
+
+// Define size() method
+int Stack::size() {
+  return capacity;
+}
+ 
+
+// Define push() method
+void Stack::push(int x) {
+  // Check if stack overflows
+  if (isFull()) {
+    std::cout << "Stack overflows. Program terminated." << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  // Push
+  std::cout << "Pushing " << x << std::endl;
+  arr[++top] = x;
+}
+
+
+// Define isFull() method
+bool Stack::isFull() {
+  return (top >= (capacity-1));
+}
 
 
 
@@ -47,9 +89,11 @@ int main() {
   using std::cout;
   using std::endl;
 
-  int chosen_size = 5;
+  int chosen_size = 6;
   Stack myStack(chosen_size); // if size not specified, Stack
 			      // constructor will use DEFAULT_SIZE???
+
+  cout << "Stack size is " << myStack.size() << endl;
 
 
   return 0;
